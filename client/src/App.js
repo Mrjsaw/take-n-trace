2d99e5bfebf42e84cddaa2ad22c7e1136118bad7
Seppe Coninx <seppe.coninx@telenet.be>
2019-12-04 13:15:37 +0100

    Added a basic home page + footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 class App extends Component {
   state = {
-      data: null
-    };
-  
-    componentDidMount() {
-        // Call our fetch function below once the component mounts
-      this.callBackendAPI()
-        .then(res => this.setState({ data: res.express }))
-        .catch(err => console.log(err));
-    }
-      // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callBackendAPI = async () => {
-      const response = await fetch('/express_backend');
-      const body = await response.json();
-  
-      if (response.status !== 200) {
-        throw Error(body.message) 
-      }
-      return body;
-    };
-  
-    render() {
-      return (
-        <div className="App">
-          <header>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">{this.state.data}</p>
-        </div>
-      );
-    }
+    data: null
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
   }
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Center">
+          <form>
+            <input className="TrackNr" type="text" name="tracknr" placeholder="Tracking number"/>
+            <input type="submit" value="Track" />
+          </form>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <h4>&copy; Take 'n Trace 2019</h4>
+        </div>
+    );
+}
+
+export default Footer;
