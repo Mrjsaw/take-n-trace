11fd6afe54afa9983127c89631afb68c28f5f7f8
zerotwo0 <58424297+zerotwo0@users.noreply.github.com>
2019-12-05 12:07:20 +0100

    initial commit + header + login form in react

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export const Home = () => (
+
+        <div>
+
+            <h2>Welcome to the Home page</h2>
+            <p>
+            Maecenas vehicula ac libero ac vehicula. Vestibulum varius ante enim, sed rutrum velit tristique ac. Pellentesque ullamcorper congue ex vel semper. Sed nec arcu ipsum. Sed dictum tempor lacinia. Ut metus nulla, vehicula mattis erat eu, auctor gravida nunc. Donec posuere, massa in dapibus efficitur, lectus odio tempor leo, non condimentum magna nisl eu arcu. Quisque ac congue dolor. In eget efficitur ex. Proin sem augue, vestibulum sed posuere vel, pretium ut tortor. Duis id enim a ex facilisis gravida. Maecenas ac commodo ipsum. Ut aliquet augue sit amet bibendum dictum. Duis pretium, dolor ac pulvinar ornare, ipsum velit vulputate enim, in convallis eros eros id ipsum.
+
+Praesent mollis purus in mauris sollicitudin, ut fringilla libero posuere. Suspendisse massa odio, blandit in ultrices in, bibendum at tortor. Cras sed odio enim. Maecenas tempor libero eu elit aliquam lacinia. Sed luctus a risus porttitor pretium. Integer faucibus nisi lorem, a fringilla arcu finibus non. Suspendisse elementum augue massa, pretium fermentum ex iaculis ut. Aenean faucibus nulla nunc, eu lacinia ex porta vel. Donec eget lorem sed ligula ullamcorper vulputate nec bibendum enim. Pellentesque consectetur odio non dictum semper. Donec interdum quam est, sit amet ultricies justo convallis id. Quisque vehicula, purus vel vehicula elementum, elit elit commodo quam, eu molestie tortor diam id turpis. Pellentesque vitae venenatis ante. In interdum vitae diam vitae maximus. Vestibulum mattis erat sem, at malesuada dolor volutpat eu.
+
+Phasellus magna turpis, sodales et hendrerit non, malesuada et massa. Mauris sollicitudin, metus sit amet volutpat euismod, mauris neque feugiat sapien, id congue est nisl et velit. Phasellus in tempus lacus, vitae pellentesque sapien. Sed rhoncus dignissim eleifend. Morbi rutrum feugiat sapien. Aliquam mollis ipsum at tortor tempus consectetur. Ut et porta ligula. Quisque at placerat lectus. Aliquam commodo lacinia odio, vel pellentesque dolor tincidunt at. In eget ipsum eget dui suscipit aliquet. Quisque id tortor id magna pharetra rhoncus. Suspendisse at auctor est. 
+            </p>
+
+        </div>
+)
