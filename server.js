abe232a86d6196ab1f2386193b410d413fb33f91
Seppe Coninx <seppe.coninx@telenet.be>
2019-12-06 13:13:02 +0100

    Database connection + getPackageById + createPackage

--- server.js
+++ server.js
@@ -1,11 +1,69 @@
 const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3010;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const connection = mysql.createConnection({
+  host: 'dt5.ehb.be',
+  user: '1920SP2TAKENTRACE',
+  password: 'INGn3hY5L',
+  database: '1920SP2TAKENTRACE'
+})
 
-// create a GET route
+//connection.connect();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
+});
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+//console.log that your server is up and running
+app.listen(port, () => console.log(`Take 'n Trace web server running on port ${port}`));
+
+//create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+//GET all package information by ID
+app.post('/getPackageById', (req, res) => {
+  connection.query(
+      'SELECT * FROM Packages WHERE id = ?',
+      [req.body.packageid],
+      function(err, results) {
+          if(err) {
+              res.send(err);
+          }
+          else {
+              console.log(results);
+              res.send(results);
+          }
+      }
+  );
+});
+
+//Create package
+app.post('/createPackage', (req, res) => {
+  connection.query(
+      'INSERT INTO `Packages` (id, trackingnumber, description, length, height, width, weight, origin, destination, status, type, date, email) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [req.body.trackingnumber, req.body.description, req.body.length, req.body.height, req.body.width, req.body.weight, req.body.origin, req.body.destination, req.body.status, req.body.type, req.body.date, req.body.email],
+      function(err, results, fields) {
+          if(err){
+              res.send(err);
+          }
+          else{
+            console.log(results);
+            res.send(results);
+          }
+      }
+  );
 });
