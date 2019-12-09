1e12c54efe393bc28f829cf6aa8bc71ded57b9d3
yassblass <50370602+yassblass@users.noreply.github.com>
2019-12-09 10:57:25 +0100

    Bootstrap CDN toegevoegd, CardComponent + shippingOptions pagina toegevoegd(tijdelijk).

---
+++ client/src/components/cardComponent.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import ShippingOptions from "../pages/shippingOptions";
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: "" };
+  }
+
+  handleSubmit = event => {
+    this.setState({});
+    alert("Hello world");
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <CardDeck>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This card has even longer
+                  content than the first to show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+          <br />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Form;
