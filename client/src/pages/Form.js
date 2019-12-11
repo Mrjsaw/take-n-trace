bc9c638871a860a77605769041fb68b151870249
yassblass <50370602+yassblass@users.noreply.github.com>
2019-12-11 15:38:53 +0100

    Changed form.js page.

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -1,39 +1,21 @@
-import React, { Component } from 'react';
-import Form from '../components/FormComponent'
+import React, { Component } from "react";
+import Form from "../components/FormComponent";
 
 class InputForm extends Component {
   state = {
     data: null
   };
 
-  componentDidMount() {
-    // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  };
-      // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-  };
-
   render() {
     return (
       <div>
         <div className="container">
-          <Form/>
+          <Form />
           <p className="">{this.state.data}</p>
         </div>
       </div>
     );
   }
 }
-
 
 export default InputForm;
